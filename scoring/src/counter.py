5ffb0467c24c518e58d194a0a615e3183382942c
Minjoon Jeong <davidminjoon@gmail.com>
2025-06-23 19:44:06 +0900

    Dialogue file for chat log analysis

---
+++ scoring/src/counter.py
@@ -0,0 +1,129 @@
+from dialogue import Dialogue
+
+
+class Counter:
+    def __init__(self, txt_file_path: str, start_yyyymmdd: int = None, end_yyyymmdd: int = None):
+        self.merge: list[Dialogue] = []
+        self.by_date: dict[str, list[Dialogue]] = {}
+        self.by_speaker: dict[str, list[Dialogue]] = {}
+
+        __in_select = start_yyyymmdd is None
+        __start_rich = f'{start_yyyymmdd // 10000}년 {(start_yyyymmdd % 10000) // 100}월 {start_yyyymmdd % 100}일' \
+            if start_yyyymmdd is not None else 'S_None'
+        __end_rich = f'{end_yyyymmdd // 10000}년 {(end_yyyymmdd % 10000) // 100}월 {end_yyyymmdd % 100}일' \
+            if end_yyyymmdd is not None else 'E_None'
+        __date = "Unknown!@#$%^&*"
+
+        with open(txt_file_path, 'r', encoding='utf-8') as f:
+            for dialogue_chunk in f.read().replace('--------------- ', '[[Date divider ').split('\n['):
+                if dialogue_chunk.find('Date divider ') != -1:
+                    # Found date indicator: update date
+                    __temp = Dialogue('[' + dialogue_chunk)
+                    __date = __temp.date_info
+                    if __date.find(__start_rich) != -1: __in_select = True
+                    if __date.find(__end_rich) != -1: __in_select = False
+
+                # Get a chunk of a potential dialogue
+                if __in_select: self.merge.append(Dialogue('[' + dialogue_chunk))
+
+        # Assign date and sort
+        current_date: str = 'Unknown!@#$%^&*'
+        for dialogue in self.merge:
+            if dialogue.date_info.startswith("Unknown"): dialogue.date_info = current_date
+            else: current_date = dialogue.date_info
+
+            # Sort by date
+            if current_date not in self.by_date.keys(): self.by_date[current_date] = []
+            self.by_date[current_date].append(dialogue)
+
+            # Sort by speaker
+            if dialogue.speaker not in self.by_speaker.keys(): self.by_speaker[dialogue.speaker] = []
+            self.by_speaker[dialogue.speaker].append(dialogue)
+
+    def assign_birthtime_scores(self) -> dict[str, dict[str, int]]:
+        __BIRTHTIME_ORDER = ['오전 솔시', '오전 설시', '오전 행시', '오전 쮸시', '오전 뀨시', '오전 릴시',
+                             '오후 솔시', '오후 설시', '오후 행시', '오후 쮸시', '오후 뀨시', '오후 릴시']
+        __BIRTHTIME_TABLE = {'오전 솔시': ((0, 28), {'솔', '배이'}),
+                             '오전 설시': ((1, 26), {'설', '설윤'}),
+                             '오전 행시': ((2, 25), {'행', '해원'}),
+                             '오전 쮸시': ((4, 13), {'쮸', '지우'}),
+                             '오전 뀨시': ((5, 26), {'뀨', '규진'}),
+                             '오전 릴시': ((10, 17), {'릴', '릴리'}),
+                             '오후 솔시': ((12, 28), {'솔', '배이'}),
+                             '오후 설시': ((13, 26), {'설', '설윤'}),
+                             '오후 행시': ((14, 25), {'행', '해원'}),
+                             '오후 쮸시': ((16, 13), {'쮸', '지우'}),
+                             '오후 뀨시': ((17, 26), {'뀨', '규진'}),
+                             '오후 릴시': ((22, 17), {'릴', '릴리'})}
+
+        assignment: dict[str, dict[str, int]] = {}
+
+        for date, dialogues in self.by_date.items():
+            current_time_whole = 0
+            __tdesc = __BIRTHTIME_ORDER[0]
+            __twhole = Dialogue.hm_to_whole(__BIRTHTIME_TABLE[__tdesc][0])
+            __success_temp = []
+
+            for dialogue in dialogues:
+                # Whenever the clock ticks by at least a minute
+                if current_time_whole < dialogue.time_whole:
+                    current_time_whole = dialogue.time_whole
+
+                    # When a birthtime passes, assign scores
+                    if current_time_whole > __twhole:
+                        if len(__success_temp) != 0:
+                            # Assign first birthtime success (+2)
+                            __birthtime_asg = {k: (1 if __twhole < 720 else 0) for k in __success_temp}
+                            __birthtime_asg[__success_temp[0]] += 2
+                            __birthtime_asg[__success_temp[-1]] += 2
+                            if len(__success_temp) >= 3: __birthtime_asg[__success_temp[2]] += 1
+                            assignment[f'{date} {__tdesc}'] = __birthtime_asg
+
+                        # Rotate birthtime agendas until a valid target (1338: One minute after Lily PM)
+                        __BIRTHTIME_ORDER.append(__BIRTHTIME_ORDER.pop(0))
+                        __tdesc = __BIRTHTIME_ORDER[0]
+                        __twhole = Dialogue.hm_to_whole(__BIRTHTIME_TABLE[__tdesc][0])
+                        while __twhole < current_time_whole < 1338:
+                            __BIRTHTIME_ORDER.append(__BIRTHTIME_ORDER.pop(0))
+                            __tdesc = __BIRTHTIME_ORDER[0]
+                            __twhole = Dialogue.hm_to_whole(__BIRTHTIME_TABLE[__tdesc][0])
+
+                        __success_temp.clear()
+
+                # Check for birthtime success
+                if __twhole == current_time_whole:
+                    if dialogue.birthtime_challenge(*__BIRTHTIME_TABLE[__tdesc]):
+                        __success_temp.append(dialogue.speaker)
+
+        return assignment
+
+    def assign_communication_scores(self) -> dict[str, int]:
+        assignment: dict[str, int] = {s: (len(d) // 22) * 2 for s, d in self.by_speaker.items()
+                                      if not s.startswith("Server")}
+        return assignment
+
+    def assign_photo_link_scores(self) -> dict[str, int]:
+        count: dict[str, int] = {s: 0 for s in self.by_speaker.keys()}
+        for speaker, dialogs in self.by_speaker.items():
+            for dialog in dialogs:
+                count[speaker] += dialog.get_photo_count()
+                count[speaker] += len(dialog.get_link_domain())
+
+        return {s: (5 if count[s] >= 60 else 0) for s in self.by_speaker.keys() if not s.startswith("Server")}
+
+    def score_total(self) -> dict[str, int]:
+        total = {s: 0 for s in self.by_speaker.keys()}
+        for birthtime in self.assign_birthtime_scores().values():
+            for speaker, score in birthtime.items():
+                total[speaker] += score
+
+        for speaker, score in self.assign_communication_scores().items(): total[speaker] += score
+        for speaker, score in self.assign_photo_link_scores().items(): total[speaker] += score
+
+        return total
+
+
+if __name__ == '__main__':
+    c = Counter('../dat/KakaoTalk_20250621_1444_43_590_group.txt')
+    for k, v in c.assign_birthtime_scores().items():
+        print(f'{k}: {v}')
---
+++ scoring/src/dialogue.py
@@ -0,0 +1,114 @@
+from __future__ import annotations
+from re import match
+
+
+class Dialogue:
+    def __init__(self, log_text: str):
+        self.dialogue: str = ''
+        self.speaker: str = "Server!@#$%^&*"
+        self.time_text: str = "Server!@#$%^&*"
+        self.date_info: str = 'Unknown!@#$%^&*'
+        self.morning_text: bool = True
+        self.time_hour, self.time_min, self.time_whole = 0, 0, 0
+        self.time_hm = (0, 0)
+
+        for line in log_text.split('\n'):
+            # Sort out server-side messages
+            if line.endswith('님이 들어왔습니다.'): continue
+            if line.endswith('님이 나갔습니다.'): continue
+            if line.endswith('님이 부방장이 되었습니다.'): continue
+            if line.endswith('님이 부방장에서 해제되었습니다.'): continue
+            if line.endswith('개의 메시지를 가렸습니다.'): continue
+
+            # Preserve date info
+            if line.startswith('[[Date divider ') and line.endswith('---------------'):
+                self.date_info = line[15: -16]
+                continue
+
+            # If this line is the start of a dialogue, fetch dialogue time and speaker
+            char_ptr = 0
+            if line.startswith('['):
+                # Fetch dialogue speaker
+                char_ptr = line.find(']')
+                if char_ptr == -1: continue
+                self.speaker: str = line[1: char_ptr]
+
+                # Fetch time text
+                __ochar = char_ptr
+                char_ptr = line.find(']', __ochar + 2)
+                if char_ptr == -1: continue
+                self.time_text: str = line[__ochar + 3: char_ptr]
+
+                # Parse time text
+                __colon_idx = self.time_text.index(':')
+                __txth, __txtm = int(self.time_text[3: __colon_idx]) % 12, int(self.time_text[__colon_idx + 1:])
+                self.morning_text: int = (self.time_text[1] == '전')
+                self.time_hour: int = int(not self.morning_text) * 12 + __txth
+                self.time_minute: int = __txtm
+                self.time_hm: tuple[int, int] = (self.time_hour, self.time_minute)
+                self.time_whole: int = self.time_hour * 60 + self.time_minute
+
+                # Set character pointer to start of dialogue
+                char_ptr += 2
+
+            # Fetch dialogue
+            self.dialogue += line[char_ptr:] + '\n'
+
+        # Trim the final line break
+        self.dialogue = self.dialogue[:-1]
+
+    @staticmethod
+    def hm_to_whole(time_hm: tuple[int, int]) -> int:
+        return time_hm[0] * 60 + time_hm[1]
+
+    @staticmethod
+    def whole_to_hm(time_whole: int) -> tuple[int, int]:
+        return divmod(time_whole, 60)
+
+    def birthtime_challenge(self, birthtime_hm: tuple[int, int], test_chars: set[str]) -> bool:
+        """
+        Returns True if this dialogue was sent precisely at the time (``birthtime_hm[0]``: ``birthtime_hm[1]``)
+        and contains at least one of the characters in ``test_chars``.
+        :param birthtime_hm: A tuple of (hour, minute) to test for.
+        :param test_chars: A set of single-letter characters to test for.
+        :return: True if the birthtime challenge conditions are all satisfied. False otherwise.
+        """
+        # Check time constraint
+        if self.hm_to_whole(birthtime_hm) != self.time_whole: return False
+
+        # Check letter containment constraint
+        return len(set(self.dialogue).intersection(test_chars)) != 0
+
+    def get_link_domain(self) -> list[str]:
+        """
+        If the dialogue contains links, returns their domains. (specified by "https://youtube.com/...")
+        :return: If the dialogue contains links, a list of their domains. Empty list otherwise.
+        """
+        __ret_list: list[str] = []
+        __domain_end = 0
+        while __http_idx := self.dialogue.find('https://', __domain_end) + 8:
+            if __http_idx == 7: return __ret_list
+            __domain_end = self.dialogue.find('/', __http_idx)
+            __ret_list.append(self.dialogue[__http_idx: __domain_end])
+
+    def get_photo_count(self) -> int:
+        """
+        If the dialogue is a photo send, returns their count.
+        :return: If the dialogue is a photo send, its count. 0 otherwise.
+        """
+        if not match(r'^사진( [0-9]{1,2}장)?$', self.dialogue): return 0
+        if not self.dialogue.endswith('장'): return 1
+        return int(self.dialogue[3: -1])
+
+    def __eq__(self, other: Dialogue) -> bool:
+        return __eq__(self.time_whole, other.time_whole)
+
+    def __gt__(self, other: Dialogue) -> bool:
+        return __gt__(self.time_whole, other.time_whole)
+
+    def __str__(self):
+        return f'[{self.speaker}] [{self.time_text}] {self.dialogue}'
+
+    def __repr__(self):
+        return self.__str__()
+
